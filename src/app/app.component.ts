7237c3d141597633db87adca503aa235b2dc7ab9
Suraj Vishwakarma <surajvishwakarma@protonmail.com>
2023-06-11 12:42:53 +0530

    ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './services/post.service';
+
+interface Student {  
+  id: Number;  
+  name: String;  
+  email: String;  
+  gender: String;  
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'KodingNotesApp';
+  posts:any;
+  
+  constructor(private service:PostService) {}
+  
+  ngOnInit() {
+      this.service.getPosts()
+        .subscribe(response => {
+          this.posts = response;
+          console.log(response)
+        });
+  }
 }
